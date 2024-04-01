7932e7b8df5d22df5c823d306943cb4240f8b963
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-04-01 16:53:39 -0500

    jajja

--- src/layouts/auth/Login.jsx
+++ src/layouts/auth/Login.jsx
@@ -1,13 +1,22 @@
 import userLogo from "../../assets/user.avif";
+import { useState } from "react";
 
-const Login = () => {   
+const Login = () => { 
+    const[getUsuario,setUsuario] = useState(null);
+    const[getContrasena,setContrasena] = useState(""); 
+    const[getCorreo,setCorreo] = useState(""); 
+
+    
+
+    console.log(getUsuario)
+ 
   return (
     <form className="login-form">
       <img src={userLogo} alt="Usuario" />
       <h2>Iniciar sesión</h2>
       <div className="input-group">
         <label htmlFor="email">Correo electrónico:</label>
-        <input type="text" />
+        <input value={getUsuario} type="text" name="" id="" onChange={function name(e) {setUsuario(e.target.value)}} />
       </div>
       <div className="input-group">
         <label htmlFor="password">Contraseña:</label>
